00f021619c04b33f9d7cd5a81bb1fe7497c96fd8
ZhuChenguang <915373240@qq.com>
2025-04-10 02:10:09 +0800

    test: 14

--- controller/tiptap.js
+++ controller/tiptap.js
@@ -3,7 +3,7 @@
  * @Author: 朱晨光
  * @Date: 2023-12-02 21:16:00
  * @LastEditors: cg
- * @LastEditTime: 2025-04-10 02:08:03
+ * @LastEditTime: 2025-04-10 02:10:03
  */
 // t-token markdown页token
 import koaRouter from "koa-router";
@@ -170,17 +170,21 @@ tiptap_router.post("/saveData", async (ctx, next) => {
 
 tiptap_router.get("/getData", async (ctx, next) => {
   if (ctx.fail) return await next();
-  try {
-    const data = await tiptap_db.getData("/data");
-    ctx.success = {
-      msg: "保存成功",
-      data
-    };
-  } catch {
-    ctx.fail = {
-      msg: "获取失败",
-    };
-  }
+  // try {
+  //   const data = await tiptap_db.getData("/data");
+  //   ctx.success = {
+  //     msg: "保存成功",
+  //     data
+  //   };
+  // } catch {
+  //   ctx.fail = {
+  //     msg: "获取失败",
+  //   };
+  // }
+  ctx.success = {
+    msg: "保存成功",
+    data
+  };
   await next();
 });
 
